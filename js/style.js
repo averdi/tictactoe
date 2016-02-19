f8e385df970dc8d17e2a9643081cc9e88f181653
Antonia Verdi <averdi@gm.slc.edu>
2016-02-19 10:43:41 -0600

    when you click on a square of the board an image shows. yess

---
+++ js/style.js
@@ -0,0 +1,9 @@
+$(function(){
+
+$("#cell-11, #cell-12, #cell-13, #cell-21, #cell-22, #cell-23, #cell-31, #cell-32, #cell-33").click(function(){
+      var cell = $(this);
+            cell.css("background-image", "url(./images/letter-x.png");
+})
+
+
+});
