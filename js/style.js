a8ab87f940f9a934cfce735badacb2df23faeeae
Antonia Verdi <averdi@gm.slc.edu>
2016-02-19 20:05:09 -0600

    game is now fully operational

--- js/style.js
+++ js/style.js
@@ -11,17 +11,57 @@ $(function(){
         if(turn==0) {
           turn = 1;
           cell.addClass('x-hand');
-          //some text says that it's then player two's turn
+          $('#messages').html("Player O's turn")
         } else {
           turn = 0;
           cell.addClass('o-hand')
-          //some text says that it's then player one's turn
+          $('#messages').html("Player X's turn")
         }
+        seeWhoWon();
       }
   });
 
+function seeWhoWon(){
+  if ($('#cell-11').hasClass('x-hand') && $('#cell-12').hasClass('x-hand') && $('#cell-13').hasClass('x-hand')){
+    alert("x wins!");
+  } else if ($('#cell-11').hasClass('o-hand') && $('#cell-12').hasClass('o-hand') && $('#cell-13').hasClass('o-hand')){
+      alert("o wins!");
+  } else if ($('#cell-21').hasClass('x-hand') && $('#cell-22').hasClass('x-hand') && $('#cell-23').hasClass('x-hand')){
+      alert("x wins!");
+  } else if ($('#cell-21').hasClass('o-hand') && $('#cell-22').hasClass('o-hand') && $('#cell-23').hasClass('o-hand')){
+      alert("o wins!");
+  } else if ($('#cell-31').hasClass('x-hand') && $('#cell-32').hasClass('x-hand') && $('#cell-33').hasClass('x-hand')){
+      alert("x wins!");
+    } else if ($('#cell-31').hasClass('o-hand') && $('#cell-32').hasClass('o-hand') && $('#cell-33').hasClass('o-hand')){
+      alert("o wins!");
+    } else if ($('#cell-11').hasClass('x-hand') && $('#cell-21').hasClass('x-hand') && $('#cell-31').hasClass('x-hand')){
+      alert ("x wins!");
+    } else if ($('#cell-11').hasClass('o-hand') && $('#cell-21').hasClass('o-hand') && $('#cell-31').hasClass('o-hand')){
+      alert ("o wins!");
+    } else if ($('#cell-12').hasClass('x-hand') && $('#cell-22').hasClass('x-hand') && $('#cell-32').hasClass('x-hand')){
+      alert ("x wins!");
+    } else if ($('#cell-12').hasClass('o-hand') && $('#cell-22').hasClass('o-hand') && $('#cell-32').hasClass('o-hand')){
+      alert ("o wins!");
+    } else if ($('#cell-13').hasClass('x-hand') && $('#cell-23').hasClass('x-hand') && $('#cell-33').hasClass('x-hand')){
+      alert ("x wins!");
+    } else if ($('#cell-13').hasClass('o-hand') && $('#cell-23').hasClass('o-hand') && $('#cell-33').hasClass('o-hand')){
+      alert ("o wins!");
+    } else if ($('#cell-11').hasClass('x-hand') && $('#cell-22').hasClass('x-hand') && $('#cell-33').hasClass('x-hand')){
+      alert ("x wins!");
+    } else if ($('#cell-11').hasClass('o-hand') && $('#cell-22').hasClass('o-hand') && $('#cell-33').hasClass('o-hand')){
+      alert ("o wins!");
+    } else if ($('#cell-13').hasClass('x-hand') && $('#cell-22').hasClass('x-hand') && $('#cell-31').hasClass('x-hand')){
+      alert ("x wins!");
+    } else if ($('#cell-13').hasClass('o-hand') && $('#cell-22').hasClass('o-hand') && $('#cell-31').hasClass('o-hand')){
+      alert ("o wins!");
+    };
+};
+
+
+
   $('.button').click(function(){
     $('td').removeClass('x-hand').removeClass('o-hand');
+    $('#messages').html("Player turn: 1")
 
   });
 
